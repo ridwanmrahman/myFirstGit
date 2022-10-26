f3ee01d1a5b8d98246c8e46aef82954c70d64437
www-data <www-data@s5236144.elf.ict.griffith.edu.au>
2022-10-26 21:04:14 +1000

    added adding functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var mess = ("hello world");
-console.log(mess);
 
+
+const num1 = 5;
+const num2 = 10;
+
+// add two numbers
+const sum = num1 + num2;
+
+console.log(mess + 'The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
